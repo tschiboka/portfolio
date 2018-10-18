136985a4c9f35fcc4e97fc37d606a9dc9537c2b8
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2018-10-18 14:20:32 +0100

    nav highlights when reaches section

--- js/index.js
+++ js/index.js
@@ -46,3 +46,40 @@ function vinylArmAnimate() {
     }, 2500);
   });
 } // end of vinylArmAnimate
+
+listenActiveSection();
+
+function listenActiveSection() {
+  const header = document.querySelector("header"),
+    about = document.querySelector(".about"),
+    projects = document.querySelector(".projects"),
+    contact = document.querySelector(".contact"),
+    sectors = [header, about, projects, contact];
+
+  function activateNav(el) {
+    const list = document.querySelectorAll("nav li:not(:last-child) a");
+
+    // remove all classes
+    list.forEach(e => {
+      e.classList.remove("active");
+    });
+
+    // select active item by its index in the array
+    list[sectors.findIndex(e => e === el)].classList.add("active");
+  } // end of paintActiveNavLi
+
+  window.addEventListener("scroll", () => {
+    if (window.innerWidth < 700) return void 0;
+
+    const active = sectors
+      // section is active until reaches its 2/3
+      .map(e => {
+        const rect = e.getBoundingClientRect();
+        return [e, rect.bottom - rect.height / 3];
+      }) // end of map
+      // weed out hte ones that 2/3 is minus and get the first item
+      .filter(e => e[1] > 0)[0][0];
+
+    activateNav(active);
+  }); // end of listenActiveSection
+}
